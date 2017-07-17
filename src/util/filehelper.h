3018a71ff5f6d4145a1ba9f8b603bbaf28ae2739
YI QI <YI QI>
2017-07-17 13:12:02 +0800

    [Add resource]

--- src/util/filehelper.h
+++ src/util/filehelper.h
@@ -3,13 +3,16 @@
 
 #include <windows.h>
 
-std::string getFullPath(const char* file)
+std::string getFullPath(std::string file)
 {
     char path[MAX_PATH];
     GetModuleFileName(NULL, path, MAX_PATH);
-    (strrchr(path, '\\'))[1] = '\0';
-    lstrcat(path, file);
-    return std::string(path);
+    std::string fullPath(path);
+    size_t index = fullPath.find("ffmpeg_study");
+    fullPath.erase(fullPath.begin() + index + 13, fullPath.end());
+    fullPath.append("resource\\");
+    fullPath.append(file);
+    return std::string(fullPath);
 }
 
 
